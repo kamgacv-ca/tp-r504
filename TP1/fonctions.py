b09ff764a271ff06e34a39918492e1cee5d33f09
kamgacv-ca <kamgacv@gmail.com>
2025-09-05 11:04:50 +0200

    modifie test et fohnction

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,8 +1,18 @@
+def puissance(a, b):
+    if not type(a) is int or not type(b) is int:
+        raise TypeError("Only integers are allowed")
 
-def puissance (a,b):
-	if not type(a) is int or not type(b) is int: 
-		raise TypeError("Only integers are allowed")
-	k = a ** b 
+    if a == 0 and b < 0:
+        raise ValueError("0 à une puissance négative est indéfini")
 
-	return k
+    result = 1
+
+    for i in range(abs(b)):
+        result *= a
+
+    if b < 0:
+        return 1 / result
+    else:
+        return result
+
 
