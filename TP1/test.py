d70677547f3f712e86b5c47cd6e9f8e74c658ae3
kamgacv-ca <kamgacv@gmail.com>
2025-09-05 10:54:20 +0200

    modifie test

--- TP1/test.py
+++ TP1/test.py
@@ -13,3 +13,17 @@ def test_2():
 	assert f.puissance(-1,-2) == 1
 	assert f.puissance(1,2) == 1
 
+#Vérifi eque x>0 on a bien 0^x=0
+
+def operation(): 
+	assert f.puissance(0,1)==0
+	assert f.puissance(0,5)==0
+	assert f.puissance(0,100)==0
+
+def test_negative_raises():
+	with pytest.raises(ValueError):
+		assert f.puissance(0,-1)
+		assert f.puissance(0,-5)
+
+
+
