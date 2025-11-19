d81c6209eefa00a2564dddec52878d66ff92919c
kamgacv-ca <kamgacv@gmail.com>
2025-11-19 11:34:52 +0100

    tp

--- TP-LB/tp-B/tp-C/appli/app.py
+++ TP-LB/tp-B/tp-C/appli/app.py
@@ -1,11 +1,10 @@
 from flask import Flask
-
+import os 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'serveur 2\n'
+    return (os.environ.get('HOSTNAME')+"2\n")
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
-
